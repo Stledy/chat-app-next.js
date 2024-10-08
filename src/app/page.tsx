66fbe9e075c5bdc501ae57afd1b49ff84a1a5d18
Stledy <152036434+Stledy@users.noreply.github.com>
2024-10-08 12:41:05 +0200

    app

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+const HomePage: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: '#09142A',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+      }}
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default HomePage;
